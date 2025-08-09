49a51d7b5edccb1e066d58d9ff7e389339308323
It's Ami <bia982497@gmail.com>
2025-08-09 17:27:55 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- src/components/icons.tsx
+++
@@ -1,8 +0,0 @@
-import type { LucideProps } from "lucide-react";
-import PlanRightLogo from "./planright-logo";
-
-export const Icons = {
-  logo: (props: LucideProps) => (
-    <PlanRightLogo {...props} />
-  ),
-};
